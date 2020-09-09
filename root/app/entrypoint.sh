5cd5934ba03ad2117e915d08a9e1dab3acf213c4
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-09 19:32:35 +0200

    Fixed issue in var -e
    Added sleep on fail to be able to read logs

--- root/app/entrypoint.sh
+++ root/app/entrypoint.sh
@@ -25,6 +25,7 @@ for i in 0 1 2 3 4 5 6 7 8 9 ; do
         if [ $? = 1 ] ; then
 
             log -e supervisor "Initialize failed on executing: $filepath"
+            sleep 5;
             exit 1;
 
         fi
